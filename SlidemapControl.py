b7f0391224fd0aeda1024874464ce94ddfd44df8
Devin Willis <gatordevin@gmail.com>
2019-08-15 18:52:43 -0400

    Updated control code starting class structure for G-Code sender

--- SlidemapControl.py
+++ SlidemapControl.py
@@ -1,29 +1,86 @@
 import serial
 import time
 
-ser = serial.Serial('/dev/ttyUSB0', 115200)
-time.sleep(1)
+class GrblComm:
 
-ser.write(b'M17\r\n')
-ser.write(b'M92 Y400\r\n')
+    def __init__(self, port, baud, reponse, debug):
+        #Initialize variables
+        self.port = port
+        self.baud = baud
+        self.buffSize = 100
+        self.response = response
+        self.debug = debug
 
-#while True:
-#    print("Please Enter Command:")
-#    command = input()
-#    if(command == "exit"):
-#        break
-#
-#    gcode = command + '\r\n'
-#    ser.write(str.encode(gcode))
-pos = 0
-while True:
-    pos += 8
-    gcode = "G0 Y" + str(pos) + '\r\n'
-    print(gcode)
-    ser.write(str.encode(gcode))
-    time.sleep(2)
+        #Connect to device running Grbl firmware
+        if(self.debug):
+            print("Connecting to Grbl")
 
-ser.write(b'M18\r\n')
+        self.ser = serial.Serial(self.port, self.baud)
+        time.sleep(1)
 
-time.sleep(0.5)
-ser.close()
+        if(self.debug):
+            print("Grbl device connected")
+
+    #Return the current baudrate
+    def getBaud(self):
+        if(self.debug):
+            print("Baudrate is " + self.baud)
+        return self.baud
+
+    #Return the current USB port
+    def getPort(self):
+        if(self.debug):
+            print("Baudrate is " + self.port)
+        return self.port
+
+    #Move device rapidly using G00 axis all move at max speed to position specified
+    def rapidMovement(self,posStr):
+        self.sendGCode('G00 ' + posStr)
+        if(self.response):
+            resp = ser.read(self.buffSize)
+            if(self.debug):
+                print(resp)
+
+    #Move device in a line using G01 axis move at the proper speed so all stop movign at the same time
+    def linearMovement(self,posStr):
+        self.sendGCode('G01 ' + posStr)
+        if(self.response):
+            resp = ser.read(self.buffSize)
+            if(self.debug):
+                print(resp)
+
+    #Inform Grbl G-Code commands will be sent in inches
+    def useInches(self):
+        self.sendGCode('G20')
+        if(self.response):
+            resp = ser.read(self.buffSize)
+            if(self.debug):
+                print(resp)
+
+    #Inform Grbl G-Code commands will be sent in mm
+    def useMM(self):
+        self.sendGCode('G21')
+        if(self.response):
+            resp = ser.read(self.buffSize)
+            if(self.debug):
+                print(resp)
+
+    #Move device to the set home position (Does not home device)
+    def returnHome(self):
+        self.sendGCode('G28')
+        if(self.response):
+            resp = ser.read(self.buffSize)
+            if(self.debug):
+                print(resp)
+
+    #Send a G-Code command
+    def sendGCode(self,cmd):
+        if(self.debug):
+            print(cmd)
+        self.ser.write(str.encode(cmd + '\r\n'))
+
+    #Disconnect from device running Grbl firmware
+    def closeSer(self):
+        if(self.debug):
+            print("Disconnceting from Grbl")
+        self.ser.close()
